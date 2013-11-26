97cd81e9e64da74967d3a16ddb34d70c143a8db0
Chimo <chimo@chromic.org>
2013-11-25 23:05:34 -0500

    Removes debugging statements

--- websockets.js
+++ websockets.js
@@ -1,11 +1,9 @@
 var wsRealtime = {
     init: function (server, port, timeline) {
-console.log('subbing to ' + timeline);
         var conn = new ab.Session(
             'ws://' + server + ':' + port,
             function () { // Connect
                 conn.subscribe(timeline, function (path, data) {
-                    console.log('New article published to category "' + path + '" : ' + JSON.stringify(data));
                     RealtimeUpdate.receive(data);
                 });
             },
