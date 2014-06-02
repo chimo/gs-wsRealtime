955c7c3dce7d869be839aa6981375d322fada785
chimo <chimo@chromic.org>
2014-06-01 22:25:29 -0400

    Lint

--- js/websockets.js
+++ js/websockets.js
@@ -1,19 +1,24 @@
-var wsRealtime = {
-    init: function (server, port, timeline) {
-        var conn = new ab.Session(
-            'ws://' + server + ':' + port,
-            function () { // Connect
-                conn.subscribe(timeline, function (path, data) {
-                    RealtimeUpdate.receive(data);
-                });
-            },
-            function () { // Connection closed
-                console.warn('WebSocket connection closed');
-            },
-            { // Additional parameters, we're ignoring the WAMP sub-protocol for older browsers
-                'skipSubprotocolCheck': true
-            }
-        );
-    }
-}
+(function () {
+    'use strict';
+    /*global ab: false, RealtimeUpdate: false */
+
+    window.wsRealtime = {
+        init: function (server, port, timeline) {
+            var conn = new ab.Session(
+                    'ws://' + server + ':' + port,
+                    function () { // Connect
+                        conn.subscribe(timeline, function (undefined, data) {
+                            RealtimeUpdate.receive(data);
+                        });
+                    },
+                    function () { // Connection closed
+                        console.warn('WebSocket connection closed');
+                    },
+                    { // Additional parameters, we're ignoring the WAMP sub-protocol for older browsers
+                        'skipSubprotocolCheck': true
+                    }
+                );
+        }
+    };
+}());
 
